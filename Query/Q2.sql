62d65ab7846a67334e91d44aba2fa2ef957c9ec7
bit-Beat <ehwns4337@naver.com>
2022-01-10 20:46:08 +0900

    README3

--- Query/Q2.sql
+++ Query/Q2.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT concept_name 
 FROM 
-(SELECT person_id, UPPER(concept_name) AS concept_name ,condition_concept_id, concept_id
+(SELECT UPPER(concept_name) AS concept_name 
 FROM condition_occurrence AS co
 INNER JOIN concept AS cpt
 ON co.condition_concept_id = cpt.concept_id) AS foo 
